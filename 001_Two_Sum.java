ef0027f0b43b6b332947843ea62cc71d022b5862
gellikeerthi <gellikeerthi07@gmail.com>
2026-06-04 20:50:52 +0530

    Implement 001_Two_Sum.java
    Removed problem metadata comments from the file.

--- 001_Two_Sum.java
+++ 001_Two_Sum.java
@@ -1,7 +1,4 @@
 // Problem: Two Sum
-// LeetCode #1
-// Difficulty: Easy
-// Date: 2026-06-04
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){

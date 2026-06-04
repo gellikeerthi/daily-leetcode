3ed041869e87fd54eeb94c88903c2066fa8a33cc
gellikeerthi <gellikeerthi07@gmail.com>
2026-06-04 20:51:58 +0530

    Implement palindrome check for integer numbers

---
+++ 002_Palindrome.java
@@ -0,0 +1,23 @@
+// Problem: Palindrome Number
+// LeetCode #9
+// Difficulty: Easy
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        else{
+        int temp=x;
+        int rev=0;
+       while(temp>0){
+        int rem=temp%10;
+        rev=rev*10+rem;
+        temp=temp/10;
+       }
+    if(rev==x){
+       return true;
+       }
+       return false;
+        }
+}
+}

9e204023b140ac21b0893d0c3a7c2ebce6b4da1c
gellikeerthi <gellikeerthi07@gmail.com>
2026-06-08 22:37:43 +0530

    Implement Remove Element solution structure
    Added problem statement and class structure for the solution.

---
+++ RemoveElement.java
@@ -0,0 +1,14 @@
+Leetcode: Remove Element
+Difficulty: easy
+Problem No:27
+class Solution {
+    public int removeElement(int[] nums, int val) {
+      int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=val){
+           nums[i]=nums[j];
+           i++;
+            }
+        }return i;
+    }
+}

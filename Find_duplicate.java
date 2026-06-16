b185c2960e08f7791bf4c8331a4f712be24f1c7b
gellikeerthi <gellikeerthi07@gmail.com>
2026-06-16 23:25:23 +0530

    Add duplicate finding logic in Find_duplicate.java
    Implement a program to find duplicate elements in an array.

---
+++ Find_duplicate.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int i;
+        int count=0;
+        for(i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+       for(i=0;i<a.length;i++){
+        for(int j=0;j<a.length;j++){
+             if(a[i]==a[j]){
+                count=a[i];;
+             }
+        }
+       }System.out.println(count);
+    }
+}

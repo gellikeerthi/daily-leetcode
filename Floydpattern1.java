2bec7b7bcce4807584dde44a053f9db9b47e44eb
gellikeerthi <gellikeerthi07@gmail.com>
2026-06-29 15:04:37 +0530

    Create Floydpattern1.java

---
+++ Floydpattern1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+k++;
+            
+        System.out.print(k+" ");
+            }System.out.println();
+        }
+    }
+}

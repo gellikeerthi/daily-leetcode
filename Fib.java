49711b6c7ba50afd6418f1665d53f65c891c7861
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-07 18:55:46 +0530

    Create Fib.java

---
+++ Fib.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+      if(n==0){
+        System.out.println("0");
+      }else if(n==1){
+       System.out.println("1");
+      }else{
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=2;i<=n;i++){
+           c=a+b;
+           a=b;
+           b=c;
+        }
+      System.out.println(c);
+    }
+}}

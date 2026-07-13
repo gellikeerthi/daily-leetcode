363757382e5c427e1c28968633a9d87bb21d1457
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-13 21:38:27 +0530

    Add HalfDiamond.java to print star patterns
    Implement Half Diamond pattern printing based on user input.

---
+++ HalfDiamond.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+         System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+System.out.print("*");
+    }
+System.out.println();
+    }
+    
+    
+    
+    }
+}

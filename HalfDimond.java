3ecf75e56cd328b98789559bafa622fc67e192cc
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-14 15:05:03 +0530

    Create HalfDimond.java

---
+++ HalfDimond.java
@@ -0,0 +1,25 @@
+     *
+     **
+     *** 
+     ****
+     *****
+     ******  
+     *****
+     ****
+     ***    
+     **
+     *
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+  for(int i=0;i<=2*n-1;i++)
+{
+int stars=i;
+if(i>n) stars=2*n-i;
+for(int j=0;j<stars;j++)
+{
+System.out.print("*");}
+System.out.println();
+}
+    }
+}

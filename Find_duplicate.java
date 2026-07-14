689f4b789738d96fc24a056a27ba7f9503c3f44a
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-14 10:05:31 +0530

    Update Find_duplicate.java

--- Find_duplicate.java
+++ Find_duplicate.java
@@ -14,7 +14,7 @@ public class Main {
             a[i]=sc.nextInt();
         }
        for(i=0;i<a.length;i++){
-        for(int j=0;j<a.length;j++){
+        for(int j=i+1;j<a.length;j++){
              if(a[i]==a[j]){
                 count=a[i];;
              }

4f9d0af0fb55d5208bed57ff3c174470ba708a91
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-14 14:42:43 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,33 @@
+    *
+   ***
+  *****
+ *******
+*********
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<2*i+1;j++){
+            System.out.print("*");
+        }
+        for(int j=0;j<n-i-1;j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    
+    
+    
+    }
+}

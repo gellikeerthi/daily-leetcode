12b704857f666e0dd218d183c561f84d1bf0e3e4
gellikeerthi <gellikeerthi07@gmail.com>
2026-07-16 00:00:24 +0530

    Create ShellSort.java

---
+++ ShellSort.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int a[]=new int[n];
+   for(int i=0;i<n;i++){
+    a[i]=sc.nextInt();
+   }
+   for(int gap=n/2;gap>=1;gap=gap/2){
+    for(int j=gap;j<n;j++){
+        for(int i=j-gap;i>=0;i=i-gap){
+            if(a[i]>a[i+gap]){
+                int temp=a[i];
+                a[i]=a[i+gap];
+                a[i+gap]=temp;
+                
+            }
+            }
+    }
+                for(int k=0;k<n;k++){
+                    System.out.print(a[k]+" ");
+                }
+                System.out.println();
+            
+        
+    }
+   }
+   
+   
+   
+    }
